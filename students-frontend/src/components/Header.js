b0fd4508995d5d910f5e22173e6b468ed5554555
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2021-03-20 14:34:27 +0700

    Setting Frontend

---
+++ students-frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <img
+          src="https://images.unsplash.com/photo-1610128114197-485d933885c5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+          width="300"
+          className="img-thumbnail"
+          style={{ marginTop: "20px" }}
+        />
+        <hr />
+        <h5>
+          <i>presents</i>
+        </h5>
+        <h1>App with React + Django</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
